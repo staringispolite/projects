7d36a94b22045922fac3037891d23f17d36b2fd5
Jonathan Howard <howard.jonathan@gmail.com>
2010-02-07 14:22:37 -0800

    initial comit of existing staringispolite.com

--- www/www.staringispolite.com/fabfile.py
+++ www/www.staringispolite.com/fabfile.py
@@ -1,6 +1,35 @@
-from fabric.api import * 
+from fabric.api import *
 
-env.hosts = ['www.indirectmessage.com']
 
-def host_type():
+env.hosts = ['www@174.143.158.94']
+
+def linux_type(): 
+    '''
+    Just connect to each server and print type of linux
+    '''
     run('uname -s')
+
+
+def prepare_deploy_www_staringispolite_com():
+    '''
+    Pack up www.staringispolite.com for sending (no tests yet)
+    '''
+    local('tar -czf /tmp/www.staringispolite.com.tgz')
+
+
+def deploy_www_staringispolite_com():
+    '''
+    Actually perform the deploy
+    '''
+    prepare_deploy_www_staringispolite_com()
+    put('/tmp/www.staringispolite.com.tgz', '/tmp/)
+    with cd('/var/www/www.staringispolite.com/')
+        run('tar -xzvf /tmp/my_project.tgz')
+
+def deploy():
+   '''
+   Deploy all properties
+   '''
+   deploy_www_staringispolite_com()
+
+
---
+++ www/www.staringispolite.com/sgb/README.txt
@@ -0,0 +1,22 @@
+----------- Overview -----------
+
+Super Graphics Bros. was designed to be a campus-themed 3D side-scroller
+based on the Mario franchise, with a twist on typical side-scroller camera motion.
+
+It stars two lovable characters you can switch between at any time:
+The University of Maryland computer graphics professors!
+(David Mount and Ahmitabh Varshney)
+
+
+--- Instructions and Support ---
+
+Open up index.html with your favorite web browser for screenshots, a 
+gameplay video, all source code for the game, and more.
+
+If you have any questions, feel free to ask!  
+I'm always available at howard.jonathan@gmail.com
+
+Regards,
+Jonathan Howard
+Designer, Lead Programmer
+Super Graphics Bros.
