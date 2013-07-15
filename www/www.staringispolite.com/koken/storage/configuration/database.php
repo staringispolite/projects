84684866ab0c7b2c4ac8abf638978c7c6921e801
Jonathan Howard <howard.jonathan@gmail.com>
2013-07-14 21:07:33 -0700

    Adding Koken CMS, all but /storage/content, since that's massive. Backed up on my NAS at /Time Machine Backups/koken

---
+++ www/www.staringispolite.com/koken/storage/configuration/database.php
@@ -0,0 +1,11 @@
+<?php
+
+	$KOKEN_DATABASE = array(
+		'driver' => 'mysqli',
+		'hostname' => 'localhost',
+		'database' => 'koken',
+		'username' => 'koken_user',
+		'password' => 'koken*pass7',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
